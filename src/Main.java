9bd23110a2b82364632c70fc0bb8fa09d312e64e
bala67-Cyber <jcbala68551@liceo.edu.ph>
2026-07-06 09:35:25 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] id = new String[10];
+        String[] name = new String[10];
+        int[] age = new int[10];
+        String[] course = new String[10];
+        double[] grade = new double[10];
+        boolean[] enrolled = new boolean[10];
+
+        int count = 0, choice;
+
+        do {
+            System.out.println("\n1.Add  2.View  3.Search  4.Stats  5.Exit");
+            System.out.print("Choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("ID: ");
+                    id[count] = sc.nextLine();
+
+                    System.out.print("Name: ");
+                    name[count] = sc.nextLine();
+
+                    System.out.print("Age: ");
+                    age[count] = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Course: ");
+                    course[count] = sc.nextLine();
+
+                    System.out.print("Grade: ");
+                    grade[count] = sc.nextDouble();
+
+                    System.out.print(" Enrolled(true/false): ");
+                    enrolled[count] = sc.nextBoolean();
+
+                    count++;
+                    break;
+
+                case 2:
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(id[i] + " | " + name[i] + " | " + grade[i]);
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID: ");
+                    String search = sc.next();
+                    for (int i = 0; i < count; i++)
+                        if (id[i].equals(search))
+                            System.out.println(name[i] + " " + grade[i]);
+                    break;
+
+                case 4:
+                    double total = 0;
+                    for (int i = 0; i < count; i++)
+                        total += grade[i];
+                    System.out.println("Students: " + count);
+                    System.out.println("Average: " + (count == 0 ? 0 : total / count));
+                    break;
+
+                case 5:
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+            }
+
+        } while (choice != 5);
+
+        sc.close();
     }
 }
