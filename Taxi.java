e4d44fd5e0308557fe5b2ac8926358c7ede5acac
bala67-Cyber <jcbala68551@liceo.edu.ph>
2026-09-24 09:18:48 +0800

    Create Taxi.java

---
+++ Taxi.java
@@ -0,0 +1,16 @@
+public class Taxi extends Ride {
+
+    public Taxi(String passenger, double km) {
+        super(passenger, km);
+    }
+
+    @Override
+    public double fare() {
+        return 45.00 + getKm() * 13.50;
+    }
+
+    @Override
+    public String vehicle() {
+        return "Taxi";
+    }
+}
